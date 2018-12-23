929ba9b3699e0d4493f64a97629f1a019a19fe3d
filmboy3 <jonathanschwartz30@gmail.com>
2018-12-23 12:56:40 -0500

    fixed messages name Types bug

--- src/js/actions/index.js
+++ src/js/actions/index.js
@@ -4,26 +4,26 @@ let nextMessageId = 0
 let nextUserId = 0
 
 export const addMessage = (message, author) => ({
-  type: types.ADD_MESSAGE,
-  id: nextMessageId++,
-  message,
-  author
+	type: types.ADD_MESSAGE,
+	id: nextMessageId++,
+	message,
+	author
 })
 
 export const addUser = name => ({
-  type: types.ADD_USER,
-  id: nextUserId++,
-  name
+	type: types.ADD_USER,
+	id: nextUserId++,
+	name
 })
 
-export const messageReceieved = (message, author) => ({
-  type: types.MESSAGE_RECEIVED,
-  id: nextMessageId++,
-  message,
-  author
+export const messageReceived = (message, author) => ({
+	type : types.MESSAGE_RECEIVED,
+	id: nextMessageId++,
+	message,
+	author
 })
 
 export const populateUsersList = users => ({
-  type: types.USERS_LIST,
-  users
+	type: types.USERS_LIST,
+	users
 })
